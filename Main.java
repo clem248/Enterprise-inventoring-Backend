7ebcce8d8f45366f9d13c2e57f085996e6945b19
Аман Конокказыев <amanccceldeym@gmail.com>
2023-12-16 18:50:34 +0600

    Add 2-x array

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+
 public class Main {
     public static void main(String args[]){
         System.out.println("Starting with creating an inventory VKR");
+        int nums[][] = new int[][] {
+                {1,1,1,},
+                {2,2,2,},
+                {3,3,3 }
+        };
+
+        for (int i = 0; i<nums.length; i++){
+            for (int j = 0; j<nums.length; j++){
+                System.out.print(nums[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
