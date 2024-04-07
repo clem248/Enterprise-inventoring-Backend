d66b7bdf1da0d36c21154e5b47ab821e27c87a2b
Аман Конокказыев <amanccceldeym@gmail.com>
2024-04-07 12:31:47 +0600

    Add repo, entity

--- src/main/java/kg/inai/inventoring/entity/Permission.java
+++ src/main/java/kg/inai/inventoring/entity/Permission.java
@@ -8,4 +8,30 @@ public class Permission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Permission(){
+    }
+
+    public Permission(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/kg/inai/inventoring/repository/ClientRepository.java
+++ src/main/java/kg/inai/inventoring/repository/ClientRepository.java
@@ -1,4 +1,9 @@
 package kg.inai.inventoring.repository;
 
-public interface ClientRepository {
+import kg.inai.inventoring.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
